b21d068bfe45bd83da3793445d89781f063887db
dongaba <83079591+dongaba@users.noreply.github.com>
2021-05-03 08:55:40 -0500

    rewrite with PowerShell

--- src/check_video.ps1
+++ src/check_video.ps1
@@ -19,9 +19,6 @@
 #
 ###################################################################################
 
-using namespace Microsoft.VisualBasic
-using namespace System.Text.RegularExpressions
-
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 #環境設定
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -42,14 +39,13 @@ Get-Content $iniFile | Where-Object { $_ -notmatch '^\s*$' } | `
 		Where-Object { !($_.TrimStart().StartsWith('^\s*;#')) } | `
 		Invoke-Expression
 
-#----------------------------------------------------------------------
-#必要モジュールの読み込み
-Add-Type -AssemblyName Microsoft.VisualBasic
-
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 #メイン処理
 Get-ChildItem $saveBasePath -Recurse -File -Include *.mp4 |`
 		ForEach-Object { 
+
+		$videoFile = $_
+		Write-Host $videoFile
 
 		$pinfo = New-Object System.Diagnostics.ProcessStartInfo
 		$pinfo.RedirectStandardOutput = $true
@@ -58,8 +54,7 @@ Get-ChildItem $saveBasePath -Recurse -File -Include *.mp4 |`
 		#$pinfo.WindowStyle = 'Hidden'
 
 		$pinfo.FileName = $ffmpegPath
-		$pinfo.Arguments = ' -v error -i "' + $_ + '" -f null - '
-		Write-Debug "ffmpeg起動コマンド:$ffmpegPath $ffmpegArgument"
+		$pinfo.Arguments = ' -v error -i "' + $videoFile + '" -f null - '
 
 		$p = New-Object System.Diagnostics.Process
 		$p.StartInfo = $pinfo
@@ -69,17 +64,29 @@ Get-ChildItem $saveBasePath -Recurse -File -Include *.mp4 |`
 		$stdout = $p.StandardOutput.ReadToEnd()
 		$stderr = $p.StandardError.ReadToEnd()
 
-		if ( $stdout -ne '' ) {
-			Write-Host "stdout: $stdout"
-		}
-		if ( $stderr -ne '' ) {
+		#if ( $stdout -ne '' ) {
+		#	Write-Host "stdout: $stdout"
+		#}
+		#if ( $stderr -ne '' ) {
+		#	Write-Host "stderr: $stderr"
+		#}
+
+		if ( $p.ExitCode -ne 0 ) {
 			Write-Host "stderr: $stderr"
+			Write-Host 'exit code: ' $p.ExitCode
+
+			#エラーが出ているファイルを覗いて書き出し
+			$pattern = $videoFile.Name
+			Write-Host $pattern
+			try {
+				(Select-String -Pattern $pattern -Path $listFile -CaseSensitive -Encoding utf8 -SimpleMatch -NotMatch).Line  | Out-File $listFile -Encoding utf8
+				Remove-Item $videoFile
+			} catch {
+				Write-Host "ファイル削除できませんでした。: $videoFile"
+			}
+
 		}
-		Write-Host 'exit code: ' $p.ExitCode
 
 	}
 
-#$ffmpegArgument = ' -v error -i "' + $_ + '" -f null - >> v:\check.log 2>&1'
-#Write-Debug "ffmpeg起動コマンド:$ffmpegPath $ffmpegArgument"
-#Start-Process -FilePath ($ffmpegPath) -ArgumentList $ffmpegArgument -PassThru -Wait
 
