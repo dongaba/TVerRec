a1b116c9de383f5e7eb67fb7355176131383b649
dongaba <83079591+dongaba@users.noreply.github.com>
2022-09-11 13:41:49 -0500

    yt-dlp to ytdl-patched

--- conf/user_setting.ps1
+++ conf/user_setting.ps1
@@ -76,7 +76,7 @@ $script:disableValidation = $false
 $script:disableUpdateYoutubedl = $false
 
 #youtube-dlの取得元
-$script:preferredYoutubedl = 'yt-dlp'	#'yt-dlp' or 'ytdl-patched'
+$script:preferredYoutubedl = 'ytdl-patched'	#'yt-dlp' or 'ytdl-patched'
 
 #ffmpegの自動アップデートを無効化
 $script:disableUpdateFfmpeg = $false
