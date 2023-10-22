e58cbf0922f08148ef54acd2e078e55aa71fcaa5
dongabacho <83079591+dongaba@users.noreply.github.com>
2023-10-22 09:25:26 -0500

    bug fix of deleting empty dirs

--- src/delete_trash.ps1
+++ src/delete_trash.ps1
@@ -228,8 +228,9 @@ showProgressToast `
 	-Silent $false
 
 $local:emptyDirs = @()
-$local:emptyDirs = @(((Get-ChildItem -LiteralPath $script:downloadBaseDir -Recurse).where({ $_.PSIsContainer -eq $true })).Where({ ($_.GetFiles().Count -eq 0) -And ($_.GetDirectories().Count -eq 0) }).FullName)
-if ($local:emptyDirs.Count -eq 0) { { Write-Warning ('❗ 空ディレクトリを見つけられませんでした') } }
+$local:emptyDirs = @((Get-ChildItem -LiteralPath $script:downloadBaseDir -Recurse).where({ $_.PSIsContainer -eq $true })).Where({ ($_.GetFiles().Count -eq 0) -And ($_.GetDirectories().Count -eq 0) })
+if ($local:emptyDirs.Count -ne 0) { $local:emptyDirs = @($local:emptyDirs.Fullname) }
+else { { Write-Warning ('❗ 空ディレクトリを見つけられませんでした') } }
 
 $local:emptyDirTotal = $local:emptyDirs.Count
 
