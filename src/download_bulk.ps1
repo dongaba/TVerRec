3848e14aed8b1688b4a6f2ab726938e0881b7fd8
likecoffee <likecoffeejp@gmail>
2023-10-30 18:34:41 +0900

    個別ダウンロードの強制モード追加

--- src/download_bulk.ps1
+++ src/download_bulk.ps1
@@ -157,7 +157,8 @@ foreach ($local:keywordName in $local:keywordNames) {
 		downloadTVerVideo `
 			-Keyword $local:keywordName `
 			-URL $local:videoLink `
-			-Link $local:videoLink.Replace('https://tver.jp', '')
+			-Link $local:videoLink.Replace('https://tver.jp', '') `
+			-Single $false
 	}
 	#----------------------------------------------------------------------
 
