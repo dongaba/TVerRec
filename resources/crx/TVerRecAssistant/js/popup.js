aa13ad0f45e2a2850c7cdd1e2d9e29b3ae182a28
dongabacho <83079591+dongaba@users.noreply.github.com>
2024-04-20 17:55:44 -0500

    update TVerRec Assistant to 0.0.2

--- resources/crx/TVerRecAssistant/js/popup.js
+++ resources/crx/TVerRecAssistant/js/popup.js
@@ -1,24 +1,19 @@
-//リロード
-function reload() {
-	//	location.reload();
-	location.href = ("https://tver.jp");
-}
-
 // ボタンクリック時に実行する処理を定義
 document.getElementById("refresh").addEventListener("click", async () => {
 	clearStorage();
 	console.log("TVerRec Assistant: platform_uid and platform_token cleared");
+
+	//ルールを有効化
 	chrome.runtime.sendMessage(
 		{ action: "enableRule", data: "" },
 		function (response) {
 			window.close();
 		}
 	);
-	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-	chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		function: reload,
-	});
+
+	//新規タブでTVerを開く
+	chrome.tabs.create({ url: "https://tver.jp" });
+
 	readStorage();
 });
 
--- resources/crx/TVerRecAssistant/js/tver.js
+++ resources/crx/TVerRecAssistant/js/tver.js
@@ -2,18 +2,21 @@
 const config = { attributes: false, childList: true, subtree: true };
 
 // ミューテーションオブザーバーのコールバック関数
-const callback = function(mutationsList, observer) {
-    for(const mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            const button = document.querySelector('.button_button__GOl5m.error-modal_button__fgiuz');
-            if (button) {
-                button.click();
-                // 必要ならば監視を解除
-                //observer.disconnect();
-                break;
-            }
-        }
-    }
+const callback = function (mutationsList, observer) {
+	for (const mutation of mutationsList) {
+		if (mutation.type === "childList") {
+			//通信エラーの閉じるボタン
+			const button = document.querySelector(
+				".button_button__GOl5m.error-modal_button__fgiuz"
+			);
+			if (button) {
+				button.click();
+				// 必要ならば監視を解除
+				//observer.disconnect();
+				break;
+			}
+		}
+	}
 };
 
 // ミューテーションオブザーバーのインスタンスを作成
