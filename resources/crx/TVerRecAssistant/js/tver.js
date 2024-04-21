0915dd03005ae8843412abe8f4579cff44031a74
dongabacho <83079591+dongaba@users.noreply.github.com>
2024-04-20 19:22:14 -0500

    reduce error

--- resources/crx/TVerRecAssistant/js/tver.js
+++ resources/crx/TVerRecAssistant/js/tver.js
@@ -1,26 +1,27 @@
-// 監視対象の設定
+//オブザーバの設定
 const config = { attributes: false, childList: true, subtree: true };
+
+//通信エラーの閉じるボタン
+const button = document.querySelector(".button_button__GOl5m.error-modal_button__fgiuz");
 
 // ミューテーションオブザーバーのコールバック関数
 const callback = function (mutationsList, observer) {
 	for (const mutation of mutationsList) {
 		if (mutation.type === "childList") {
-			//通信エラーの閉じるボタン
-			const button = document.querySelector(
-				".button_button__GOl5m.error-modal_button__fgiuz"
-			);
+
 			if (button) {
 				button.click();
-				// 必要ならば監視を解除
+				//必要がなくなったら監視を停止
 				//observer.disconnect();
+
 				break;
 			}
 		}
 	}
 };
 
-// ミューテーションオブザーバーのインスタンスを作成
+//MutationObserverのインスタンスを作成
 const observer = new MutationObserver(callback);
 
-// 監視を開始する
+//監視を開始
 observer.observe(document.body, config);
