b871b3a1fa66278d01a7c2af8b985825e32707bf
dongabacho <83079591+dongaba@users.noreply.github.com>
2024-04-28 11:18:26 -0500

    Disable Receive Segment Coalescing (RSC)

--- resources/wsb/setup/setup.cmd
+++ resources/wsb/setup/setup.cmd
@@ -1,5 +1,7 @@
 @echo off
 
+netsh int tcp set global rsc=disabled
+
 powershell Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 
 rem Download and Install WinGet & some more packages
