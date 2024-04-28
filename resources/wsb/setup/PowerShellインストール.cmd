6e9288713ca2bd119b3fe2649177feca7265cffa
dongabacho <83079591+dongaba@users.noreply.github.com>
2024-04-28 10:02:18 -0500

    probably supported ARM64

--- resources/wsb/setup/PowerShellインストール.cmd
+++ resources/wsb/setup/PowerShellインストール.cmd
@@ -1,20 +1,41 @@
 @echo off
 
 powershell Write-Output 'まずはWinGetをインストールするために必要なソフトウェアをインストールします...'
+if "%PROCESSOR_ARCHITECTURE%" EQU "AMD64" (
+	powershell Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile %TEMP%\Microsoft.VCLibs.x64.14.00.Desktop.appx
+	powershell Add-AppxPackage %TEMP%\Microsoft.VCLibs.x64.14.00.Desktop.appx
+	del /q %TEMP%\Microsoft.VCLibs.x64.14.00.Desktop.appx
 
-powershell Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile %TEMP%\Microsoft.VCLibs.x64.14.00.Desktop.appx
-powershell Add-AppxPackage %TEMP%\Microsoft.VCLibs.x64.14.00.Desktop.appx
-del /q %TEMP%\Microsoft.VCLibs.x64.14.00.Desktop.appx
+	powershell Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile %TEMP%\Microsoft.UI.Xaml.2.8.x64.appx
+	powershell Add-AppxPackage %TEMP%\Microsoft.UI.Xaml.2.8.x64.appx
+	del /q %TEMP%\Microsoft.UI.Xaml.2.8.x64.appx
+)
+if "%PROCESSOR_ARCHITECTURE%" EQU "IA64" (
+	powershell Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile %TEMP%\Microsoft.VCLibs.x64.14.00.Desktop.appx
+	powershell Add-AppxPackage %TEMP%\Microsoft.VCLibs.x64.14.00.Desktop.appx
+	del /q %TEMP%\Microsoft.VCLibs.x64.14.00.Desktop.appx
 
-powershell Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile %TEMP%\Microsoft.UI.Xaml.2.8.x64.appx
-powershell Add-AppxPackage %TEMP%\Microsoft.UI.Xaml.2.8.x64.appx
-del /q %TEMP%\Microsoft.UI.Xaml.2.8.x64.appx
+	powershell Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile %TEMP%\Microsoft.UI.Xaml.2.8.x64.appx
+	powershell Add-AppxPackage %TEMP%\Microsoft.UI.Xaml.2.8.x64.appx
+	del /q %TEMP%\Microsoft.UI.Xaml.2.8.x64.appx
+)
+if "%PROCESSOR_ARCHITECTURE%" EQU "ARM64" (
+	powershell Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.arm64.14.00.Desktop.appx -OutFile %TEMP%\Microsoft.VCLibs.arm64.14.00.Desktop.appx
+	powershell Add-AppxPackage %TEMP%\Microsoft.VCLibs.arm64.14.00.Desktop.appx
+	del /q %TEMP%\Microsoft.VCLibs.arm64.14.00.Desktop.appx
 
+	powershell Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.arm64.appx -OutFile %TEMP%\Microsoft.UI.Xaml.2.8.arm64.appx
+	powershell Add-AppxPackage %TEMP%\Microsoft.UI.Xaml.2.8.arm64.appx
+	del /q %TEMP%\Microsoft.UI.Xaml.2.8.arm64.appx
+)
+
+powershell Write-Output '次にWinGetをインストールします...'
 powershell Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile %TEMP%\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 powershell Add-AppxPackage %TEMP%\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 del /q %TEMP%\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 
-powershell Write-Output '次にPowerShellをインストールします...'
+
+powershell Write-Output '最後にPowerShellをインストールします...'
 winget install Microsoft.PowerShell --accept-source-agreements --accept-package-agreements
 
 mshta vbscript:execute("MsgBox(""PowerShellのインストールが完了しました。""):close")
