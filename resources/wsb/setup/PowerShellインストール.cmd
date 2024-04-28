4d4947a76cf4eafa90a49a21bfed25c4e6f4d2bc
dongabacho <83079591+dongaba@users.noreply.github.com>
2024-04-27 23:29:59 -0500

    use %TEMP% dir for temporary files

--- resources/wsb/setup/PowerShellインストール.cmd
+++ resources/wsb/setup/PowerShellインストール.cmd
@@ -2,17 +2,17 @@
 
 powershell Write-Output 'まずはWinGetをインストールするために必要なソフトウェアをインストールします...'
 
-powershell Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-powershell Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-del /q Microsoft.VCLibs.x64.14.00.Desktop.appx
+powershell Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile %TEMP%\Microsoft.VCLibs.x64.14.00.Desktop.appx
+powershell Add-AppxPackage %TEMP%\Microsoft.VCLibs.x64.14.00.Desktop.appx
+del /q %TEMP%\Microsoft.VCLibs.x64.14.00.Desktop.appx
 
-powershell Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
-powershell Add-AppxPackage Microsoft.UI.Xaml.2.8.x64.appx
-del /q Microsoft.UI.Xaml.2.8.x64.appx
+powershell Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile %TEMP%\Microsoft.UI.Xaml.2.8.x64.appx
+powershell Add-AppxPackage %TEMP%\Microsoft.UI.Xaml.2.8.x64.appx
+del /q %TEMP%\Microsoft.UI.Xaml.2.8.x64.appx
 
-powershell Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-powershell Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-del /q Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+powershell Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile %TEMP%\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+powershell Add-AppxPackage %TEMP%\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+del /q %TEMP%\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 
 powershell Write-Output '次にPowerShellをインストールします...'
 winget install Microsoft.PowerShell --accept-source-agreements --accept-package-agreements
