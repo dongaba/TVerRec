cf95845f3dadf5ca41b949e9888c1097256b45fa
dongabacho <83079591+dongaba@users.noreply.github.com>
2024-11-16 20:29:41 -0600

    update TVerRec Assistant

--- resources/crx/TVerRecAssistant/js/popup.js
+++ resources/crx/TVerRecAssistant/js/popup.js
@@ -1,18 +1,17 @@
 // ストレージからデータを読み込む関数
 function readStorage() {
 	chrome.storage.local.get(null, (data) => {
-		const { key_uid, key_token } = data;
-		if (key_uid && key_token) {
+		const { key_uid, key_token, key_sid } = data;
+		if ((key_uid && key_token) || (key_sid)) {
 			console.log(`TVerRec Assistant: platform_uid = ${key_uid}`);
 			console.log(`TVerRec Assistant: platform_token = ${key_token}`);
+			console.log(`TVerRec Assistant: member_sid = ${key_sid}`);
 			const uidElement = document.getElementById("uid");
 			const tokenElement = document.getElementById("token");
-			if (uidElement) {
-				uidElement.innerText = `platform_uid : ${key_uid}`;
-			}
-			if (tokenElement) {
-				tokenElement.innerText = `platform_token : ${key_token}`;
-			}
+			const sidElement = document.getElementById("sid");
+			if (uidElement) {uidElement.innerText = `platform_uid : ${key_uid}`;}
+			if (tokenElement) {tokenElement.innerText = `platform_token : ${key_token}`;}
+			if (sidElement) {sidElement.innerText = `member_sid : ${key_sid}`;}
 		}
 	});
 }
@@ -25,18 +24,20 @@ function clearStorage() {
 // ボタンクリック時に実行する処理を定義
 document.getElementById("refresh").addEventListener("click", async () => {
 	clearStorage();
-	console.log("TVerRec Assistant: platform_uid and platform_token cleared");
+	console.log("TVerRec Assistant: platform_uid, platform_token and member_sid cleared");
 
 	//ルールを有効化
 	chrome.runtime.sendMessage(
-		{ action: "enableRule", data: "" },
-		function () {
-			window.close();
-		}
+		{ action: "enablePlatformRule", data: "" },
+		function () {window.close();}
+	);
+	chrome.runtime.sendMessage(
+		{ action: "enableMemberRule", data: "" },
+		function () {window.close();}
 	);
 
 	//新規タブでTVerを開く
-	chrome.tabs.create({ url: "https://tver.jp" });
+	chrome.tabs.create({ url: "https://tver.jp/mypage/fav" });
 
 	readStorage();
 });
