5d60cdd56028462b6bca22ff0423c2bed2794b09
dongabacho <83079591+dongaba@users.noreply.github.com>
2024-11-17 18:52:06 -0600

    optimize WSB env setup scripts

--- resources/wsb/setup/PowerShellインストール.cmd
+++ resources/wsb/setup/PowerShellインストール.cmd
@@ -10,47 +10,29 @@ echo.
 echo.
 echo.
 echo WinGetをインストールするために必要なソフトウェアをインストールします...
-if "%PROCESSOR_ARCHITECTURE%" EQU "AMD64" (
-	powershell Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile %TEMP%\Microsoft.VCLibs.x64.14.00.Desktop.appx
-	powershell Add-AppxPackage %TEMP%\Microsoft.VCLibs.x64.14.00.Desktop.appx
-	del /q %TEMP%\Microsoft.VCLibs.x64.14.00.Desktop.appx
 
-	powershell Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile %TEMP%\Microsoft.UI.Xaml.2.8.x64.appx
-	powershell Add-AppxPackage %TEMP%\Microsoft.UI.Xaml.2.8.x64.appx
-	del /q %TEMP%\Microsoft.UI.Xaml.2.8.x64.appx
-)
-if "%PROCESSOR_ARCHITECTURE%" EQU "IA64" (
-	powershell Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile %TEMP%\Microsoft.VCLibs.x64.14.00.Desktop.appx
-	powershell Add-AppxPackage %TEMP%\Microsoft.VCLibs.x64.14.00.Desktop.appx
-	del /q %TEMP%\Microsoft.VCLibs.x64.14.00.Desktop.appx
+echo 　WinGet PowerShell moduleをPSGalleryからインストールします...
+powershell -Command "Install-PackageProvider -Name NuGet -Force | Out-Null"
+powershell -Command "Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null"
 
-	powershell Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile %TEMP%\Microsoft.UI.Xaml.2.8.x64.appx
-	powershell Add-AppxPackage %TEMP%\Microsoft.UI.Xaml.2.8.x64.appx
-	del /q %TEMP%\Microsoft.UI.Xaml.2.8.x64.appx
-)
-if "%PROCESSOR_ARCHITECTURE%" EQU "ARM64" (
-	powershell Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.arm64.14.00.Desktop.appx -OutFile %TEMP%\Microsoft.VCLibs.arm64.14.00.Desktop.appx
-	powershell Add-AppxPackage %TEMP%\Microsoft.VCLibs.arm64.14.00.Desktop.appx
-	del /q %TEMP%\Microsoft.VCLibs.arm64.14.00.Desktop.appx
+echo 　Repair-WinGetPackageManagerコマンドレットを使用してWinGetを使用可能な状態にします...
+powershell -Command "Repair-WinGetPackageManager"
 
-	powershell Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.arm64.appx -OutFile %TEMP%\Microsoft.UI.Xaml.2.8.arm64.appx
-	powershell Add-AppxPackage %TEMP%\Microsoft.UI.Xaml.2.8.arm64.appx
-	del /q %TEMP%\Microsoft.UI.Xaml.2.8.arm64.appx
-)
+echo 　WinGetのインストールが完了しました。
 
 echo.
-echo WinGetをインストールします...
-powershell Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile %TEMP%\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-powershell Add-AppxPackage %TEMP%\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-del /q %TEMP%\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-
+echo Notepad++をインストールします...
+winget install -e --id Notepad++.Notepad++ --accept-source-agreements --accept-package-agreements --source winget
 
 echo.
-echo 最後にPowerShellをインストールします...
-winget install Microsoft.PowerShell --accept-source-agreements --accept-package-agreements
+echo VLCをインストールします...
+winget install -e --id VideoLAN.VLC --accept-source-agreements --accept-package-agreements --source winget
 
 echo.
-rem mshta vbscript:execute("MsgBox(""PowerShellのインストールが完了しました。""):close")
+echo PowerShellをインストールします...
+winget install -e --id Microsoft.PowerShell --accept-source-agreements --accept-package-agreements --source winget
+
+echo.
 powershell -Command "Add-Type -Assembly System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('PowerShellのインストールが完了しました。', 'TVerRec')"
 
 explorer.exe "C:\Users\WDAGUtilityAccount\Desktop\TVerRec\win"
